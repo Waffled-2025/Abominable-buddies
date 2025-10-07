d2f64a44c58dc8d5c190b3f66d00d66ff9a9be16
WolfWaffle <wolfewsmith@gmail.com>
2025-10-07 13:33:09 -0700

    Added gamestates

---
+++ gamestate_splashscreen.c
@@ -0,0 +1,36 @@
+//---------------------------------------------------------
+// file:	gamestate_template.c
+// author:	[NAME]
+// email:	[DIGIPEN EMAIL ADDRESS]
+//
+// brief:	template file for holding gamestate functions
+//
+// documentation link:
+// https://github.com/DigiPen-Faculty/CProcessing/wiki
+//
+// Copyright © 2020 DigiPen, All rights reserved.
+//---------------------------------------------------------
+
+#include "cprocessing.h"
+#include "gamestate_splashscreen.h"
+
+// use CP_Engine_SetNextGameState to specify this function as the initialization function
+// this function will be called once at the beginning of the program
+void gamestate_splashscreen_init(void)
+{
+	// initialize variables and CProcessing settings for this gamestate
+}
+
+// use CP_Engine_SetNextGameState to specify this function as the update function
+// this function will be called repeatedly every frame
+void gamestate_splashscreen_update(void)
+{
+	// check input, update simulation, render etc.
+}
+
+// use CP_Engine_SetNextGameState to specify this function as the exit function
+// this function will be called once just before leaving the current gamestate
+void gamestate_splashscreen_exit(void)
+{
+	// shut down the gamestate and cleanup any dynamic memory
+}
---
+++ gamestate_splashscreen.h
@@ -0,0 +1,18 @@
+//---------------------------------------------------------
+// file:	gamestate_template.h
+// author:	[NAME]
+// email:	[DIGIPEN EMAIL ADDRESS]
+//
+// brief:	header file for template gamestate
+//
+// documentation link:
+// https://github.com/DigiPen-Faculty/CProcessing/wiki
+//
+// Copyright © 2020 DigiPen, All rights reserved.
+//---------------------------------------------------------
+
+#pragma once
+
+void gamestate_splashscreen_init(void);
+void gamestate_splashscreen_update(void);
+void gamestate_splashscreen_exit(void);
