8096662224440c38858d59b5716942222a4b3a0d
WolfWaffle <wolfewsmith@gmail.com>
2025-10-15 18:49:35 -0700

    Enemy combat function - not implemented

--- gamestate_fight.c
+++ gamestate_fight.c
@@ -28,17 +28,17 @@ struct Character { // Character template for player and enemy characters
 	int manaRegen;
 	float defense;
 	int attackDamage;
-	int xPosition;
-	int yPosition;
+	float xPosition;
+	float yPosition;
 };
 
 struct Character playerOne = { "Wizard", 80, 100, 50, .0f, 10, 200, 300 }; // Initilization of the wizard player character
 struct Character playerTwo = { "Tank", 120, 30, 15, .3f, 5, 200, 800 };
 struct Character playerThree = { "Rogue", 90, 50, 0, .1f, 15, 200, 1300 };
 
-struct Character enemyOne = { "Stupid idiot 1", 100, 100, 25, 0.2f, 10, 900, 300 };
-struct Character enemyTwo = { "Stupid idiot 2", 100, 100, 25, 0.2f, 10, 900, 800 };
-struct Character enemyThree = { "Stupid idiot 3", 100, 100, 25, 0.2f, 10, 900, 1300 };
+struct Character enemyOne = { "Stupid idiot 1", 100, 100, 25, 0.2f, 5, 900, 200 };
+struct Character enemyTwo = { "Stupid idiot 2", 100, 100, 25, 0.2f, 5, 1000, 400 };
+struct Character enemyThree = { "Stupid idiot 3", 100, 100, 25, 0.2f, 5, 900, 600 };
 
 
 float selectButtonY;
@@ -56,10 +56,98 @@ int delay;
 
 
 
+void character_action_attack(struct Character* _player, struct Character* _enemy, int _attacker);
+void button_Select();
+void enemy_Select();
+void enemy_Turn();
+void character_action_heal(struct Character* _characterHealer, struct Character* _characterHealed);
 
 
 
-void character_action_attack(struct Character *_player, struct Character *_enemy, int _attacker) { // function for attacks: (attacker --- 0 means player attacks enemy, 1 is other
+
+
+void gamestate_fight_init(void)
+{
+	CP_System_SetWindowSize(1200, 800);
+	CP_Graphics_ClearBackground(CP_Color_Create(100, 100, 100, 0));
+
+	CP_Settings_TextSize(45);
+	CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_CENTER, CP_TEXT_ALIGN_V_MIDDLE);
+
+	selectButtonY = 100;
+	buttonSelectOpacity = 0;
+	actionSelect = 1;
+	enemySelect = 0;
+	characterSelectY = (float)CP_System_GetWindowHeight() / 2 - ((float)CP_System_GetWindowHeight() / 4);
+	characterSelectX = (float)CP_System_GetWindowWidth() / 2 + (float)CP_System_GetWindowWidth() / 4;
+	selectedEnemy = 1;
+
+}
+
+void gamestate_fight_update(void)
+{
+	CP_Graphics_ClearBackground(CP_Color_Create(100, 100, 100, 0));
+	
+	CP_Image_Draw(CP_Image_Load("./Assets/sneak sneak.png"), (float)CP_System_GetWindowWidth()/2 , (float)CP_System_GetWindowHeight() / 2 + ((float)CP_System_GetWindowHeight()/4), 256, 256, 255);
+	CP_Image_Draw(CP_Image_Load("./Assets/its merely a flesh wound.png"), (float)CP_System_GetWindowWidth() / 2  , (float)CP_System_GetWindowHeight() / 2 - ((float)CP_System_GetWindowHeight() / 4), 256, 256, 255);
+	CP_Image_Draw(CP_Image_Load("./Assets/YOU SHALL NOT PASS.png"), (float)CP_System_GetWindowWidth() / (12/(float)5), (float)CP_System_GetWindowHeight() / 2 , 256, 256, 255);
+
+	
+	CP_Image_Draw(CP_Image_Load("./Assets/s n o w m e n e m y.png"), enemyOne.xPosition, enemyOne.yPosition, 256, 256, 255);
+	CP_Image_Draw(CP_Image_Load("./Assets/s n o w m e n e m y.png"), enemyTwo.xPosition, enemyTwo.yPosition, 256, 256, 255);
+	CP_Image_Draw(CP_Image_Load("./Assets/s n o w m e n e m y.png"), enemyThree.xPosition, enemyThree.yPosition, 256, 256, 255);
+
+
+
+	CP_Settings_Fill(CP_Color_Create(255, 0, 0, 255));
+
+	CP_Graphics_DrawRect(100, 100, 150, 75);
+
+	CP_Graphics_DrawRect(100, 200, 150, 75);
+
+	CP_Graphics_DrawRect(100, 300, 150, 75);
+
+
+	CP_Settings_Fill(CP_Color_Create(0, 255, 0, 255));
+
+	CP_Font_DrawText("ATTACK", 100, 100);
+	CP_Font_DrawText("DEFEND", 100, 200);
+	CP_Font_DrawText("REST", 100, 300);
+	
+
+
+
+
+	if (actionSelect) {
+
+		buttonSelectOpacity = 255;
+		button_Select(selectButtonY);
+		CP_Image_Draw(CP_Image_Load("./Assets/button_select.png"), 100, selectButtonY, 128*1.5, 128*1.5, 255);
+		buttonSelectOpacity = 0;
+	}
+	if (enemySelect) {
+
+		enemySelectOpacity = 255;
+		enemy_Select(characterSelectX, characterSelectY);
+		CP_Image_Draw(CP_Image_Load("./Assets/character_select.png"), characterSelectX, characterSelectY, 128 * 1.5, 128 * 1.5, 255);
+		enemySelectOpacity = 0;
+	}
+
+
+
+
+
+}
+
+
+void gamestate_fight_exit(void)
+{
+}
+
+
+
+
+void character_action_attack(struct Character* _player, struct Character* _enemy, int _attacker) { // function for attacks: (attacker --- 0 means player attacks enemy, 1 is other
 	switch (_attacker) {
 	case 0: // Enemy loses health = to player characters attack damage (min = attackDamage, max = attackDamage + 10, eg 15 - 25 or 30 - 40)
 		_enemy->health -= (rand() % ((10 + _player->attackDamage) - _player->attackDamage) + _player->attackDamage);
@@ -73,7 +161,7 @@ void character_action_attack(struct Character *_player, struct Character *_enemy
 
 
 
-void button_Select(float _selectButtonY) {
+void button_Select() {
 
 
 	int selectedAction = 1;
@@ -182,91 +270,51 @@ void enemy_Select() {
 	}
 }
 
+void enemy_Turn() {
+
+	int enemyActionChoice = (rand() % 2) + 1;
 
 
+	if (enemyActionChoice == 1) {
 
+		int enemyAttackChoice = (rand() % 3) + 1;
 
+		switch (enemyAttackChoice) {
+		case 1: 
+			character_action_attack(&playerOne, &enemyOne, 1);
+			break;
+		case 2:
+			character_action_attack(&playerTwo, &enemyOne, 1);
+			break;
+		case 3:
+			character_action_attack(&playerThree, &enemyOne, 1);
+			break;
+		}
 
-
-
-
-
-
-
-void gamestate_fight_init(void)
-{
-	CP_System_SetWindowSize(1200, 800);
-	CP_Graphics_ClearBackground(CP_Color_Create(100, 100, 100, 0));
-
-	CP_Settings_TextSize(45);
-	CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_CENTER, CP_TEXT_ALIGN_V_MIDDLE);
-
-	selectButtonY = 100;
-	buttonSelectOpacity = 0;
-	actionSelect = 1;
-	enemySelect = 0;
-	characterSelectY = (float)CP_System_GetWindowHeight() / 2 - ((float)CP_System_GetWindowHeight() / 4);
-	characterSelectX = (float)CP_System_GetWindowWidth() / 2 + (float)CP_System_GetWindowWidth() / 4;
-	selectedEnemy = 1;
-
-}
-
-void gamestate_fight_update(void)
-{
-	CP_Graphics_ClearBackground(CP_Color_Create(100, 100, 100, 0));
-	
-	CP_Image_Draw(CP_Image_Load("./Assets/sneak sneak.png"), (float)CP_System_GetWindowWidth()/2 , (float)CP_System_GetWindowHeight() / 2 + ((float)CP_System_GetWindowHeight()/4), 256, 256, 255);
-	CP_Image_Draw(CP_Image_Load("./Assets/its merely a flesh wound.png"), (float)CP_System_GetWindowWidth() / 2  , (float)CP_System_GetWindowHeight() / 2 - ((float)CP_System_GetWindowHeight() / 4), 256, 256, 255);
-	CP_Image_Draw(CP_Image_Load("./Assets/YOU SHALL NOT PASS.png"), (float)CP_System_GetWindowWidth() / (12/(float)5), (float)CP_System_GetWindowHeight() / 2 , 256, 256, 255);
-
-	
-	CP_Image_Draw(CP_Image_Load("./Assets/s n o w m e n e m y.png"), (float)CP_System_GetWindowWidth() / 2 + (float)CP_System_GetWindowWidth() / 3, (float)CP_System_GetWindowHeight() / 2, 256, 256, 255);
-	CP_Image_Draw(CP_Image_Load("./Assets/s n o w m e n e m y.png"), (float)CP_System_GetWindowWidth() / 2 + (float)CP_System_GetWindowWidth() / 4, (float)CP_System_GetWindowHeight() / 2 - ((float)CP_System_GetWindowHeight() / 4), 256, 256, 255);
-	CP_Image_Draw(CP_Image_Load("./Assets/s n o w m e n e m y.png"), (float)CP_System_GetWindowWidth() / 2 + (float)CP_System_GetWindowWidth() / 4, (float)CP_System_GetWindowHeight() / 2 + ((float)CP_System_GetWindowHeight() / 4), 256, 256, 255);
-
-
-
-	CP_Settings_Fill(CP_Color_Create(255, 0, 0, 255));
-
-	CP_Graphics_DrawRect(100, 100, 150, 75);
-
-	CP_Graphics_DrawRect(100, 200, 150, 75);
-
-	CP_Graphics_DrawRect(100, 300, 150, 75);
-
-
-	CP_Settings_Fill(CP_Color_Create(0, 255, 0, 255));
-
-	CP_Font_DrawText("ATTACK", 100, 100);
-	CP_Font_DrawText("DEFEND", 100, 200);
-	CP_Font_DrawText("REST", 100, 300);
-	
-
-
-
-
-	if (actionSelect) {
-
-		buttonSelectOpacity = 255;
-		button_Select(selectButtonY);
-		CP_Image_Draw(CP_Image_Load("./Assets/button_select.png"), 100, selectButtonY, 128*1.5, 128*1.5, 255);
-		buttonSelectOpacity = 0;
 	}
-	if (enemySelect) {
+	else if (enemyActionChoice) {
+		int enemyHealChoice = (rand() % 3) + 1;
 
-		enemySelectOpacity = 255;
-		enemy_Select(characterSelectX, characterSelectY);
-		CP_Image_Draw(CP_Image_Load("./Assets/character_select.png"), characterSelectX, characterSelectY, 128 * 1.5, 128 * 1.5, 255);
-		enemySelectOpacity = 0;
+		switch (enemyHealChoice) {
+		case 1:
+			character_action_heal(&playerOne, &enemyOne, 1);
+			break;
+		case 2:
+			character_action_heal(&playerTwo, &enemyOne, 1);
+			break;
+		case 3:
+			character_action_heal(&playerThree, &enemyOne, 1);
+			break;
+		}
 	}
 
-
-
-
-
 }
 
 
-void gamestate_fight_exit(void)
-{
+
+void character_action_heal(struct Character* _characterHealer, struct Character* _characterHealed) {
+	_characterHealed->health += (rand() % ((5 + _characterHealer->attackDamage) - _characterHealer->attackDamage) + _characterHealer->attackDamage);
 }
+
+
+
